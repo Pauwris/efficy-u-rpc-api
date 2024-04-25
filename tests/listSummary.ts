c4aba6cb3ad7b434941d974c2459be3bb0c6c524
Kristof Pauwels <kristof.pauwels@efficy.com>
2024-04-25 16:50:47 +0200

    Updated docs

--- tests/listSummary.ts
+++ tests/listSummary.ts
@@ -14,16 +14,14 @@ const crmEnv = new CrmEnv({
 
 if (typeof process.env.CRM_USER !== "string" || !process.env.CRM_USER.toLowerCase()) throw Error("Check .env configuration")
 
-
-
-test.skip('listSummary', async t => {
+test('CrmApi: listSummary Currency', async t => {
 	interface Crcy {
 		crcyName: string;
 		crcyCode: string;
 		crcySymbol: string;
 		crcyKey: UnityKey;
 	}
-	
+
 	const crm = new CrmApi(crmEnv);
 	const payload: ListSummaryPayload = {
 		fields: ["crcyName", "crcyCode", "crcySymbol", "crcyCode", "crcyKey"],
@@ -33,11 +31,31 @@ test.skip('listSummary', async t => {
 
 	try {
 		const result = await crm.listSummary<Crcy>(payload);
-		const euro = result?.list.find(item => item.crcyCode === "EUR")
-		t.assert(euro?.crcyName === "Euro", "Query Currency")
+		const euro = result?.list.find(item => item.crcyCode === "EUR");
+		t.assert(euro?.crcyName === "Euro");
 	} catch (ex) {
 		console.error(ex)
 	}
+});
 
-	t.assert(true, "listSummary");
+test('CrmApi: listSummary Company', async t => {
+	interface Company {
+		compKey: UnityKey;
+		compName: string;		
+	}
+
+	const crm = new CrmApi(crmEnv);
+	const payload: ListSummaryPayload = {
+		fields: ["compKey", "compName"],
+		tableName: "Company",
+		query: [["compArchived = 1","compName like 'Efficy%'"]]
+	};
+
+	try {
+		const result = await crm.listSummary<Company>(payload);
+		const efficyGent = result?.list.find(item => item.compName === "Efficy Gent");
+		t.assert(efficyGent?.compKey != null);
+	} catch (ex) {
+		console.error(ex)
+	}
 });
